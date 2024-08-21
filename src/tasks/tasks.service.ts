3edb853605f0eb4f2a0ea9878af0a9130827dedf
khaliliali <ali.khalili@outlook.com>
2024-08-21 11:18:49 +0800

    add validator - error handling

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -29,7 +29,12 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const taskFound = this.tasks.find((task) => task.id === id);
+
+    if (!taskFound) {
+      throw new NotFoundException(`Task with ID "${id}" not found.`);
+    }
+    return taskFound;
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
